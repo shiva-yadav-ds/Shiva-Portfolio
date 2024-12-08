66607ce10be5a58007f9ae6b232de4c133ad429e
shubhamyadav12 <shubhamyadavji678@gmail.com>
2024-12-08 15:33:30 +0530

    Add external config for react-tilt in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Create aliases for cleaner imports
-      '@': '/src',
+      '@': '/src', // Create aliases for cleaner imports
     },
   },
   server: {
     hmr: {
-      overlay: false, // Show error overlay (set to false if you want to suppress errors)
+      overlay: false, // Suppress error overlay
     },
   },
-})
+  build: {
+    rollupOptions: {
+      external: ['react-tilt'], // Add react-tilt to external configuration
+    },
+  },
+});
